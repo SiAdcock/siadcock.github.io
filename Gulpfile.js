3056ef88f964a95838536de3659ef1ef665ea2fd
Simon Adcock <simonadcock@notonthehighstreet.com>
2016-07-04 21:25:18 +0100

    Basic styles

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,19 +2,24 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
-gulp.task('serve', () => {
+gulp.task('serve', ['sass', 'copy:img'], () => {
   browserSync.init({
     server: {
       baseDir: './'
     }
   });
   gulp.watch('src/styles/**.scss', ['sass']);
+  gulp.watch('index.html', ['html']);
 });
 gulp.task('build', ['sass', 'copy:img']);
 gulp.task('sass', () => {
   gulp.src('src/styles/main.scss')
     .pipe(sass())
-    .pipe(gulp.dest('build/'))
+    .pipe(gulp.dest('build/styles/'))
+    .pipe(browserSync.stream());
+});
+gulp.task('html', () => {
+  gulp.src('index.html')
     .pipe(browserSync.stream());
 });
 gulp.task('copy:img', () => {
